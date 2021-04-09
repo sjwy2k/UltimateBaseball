9c01b2a823a904589988d4fc44892e3acd0eab9a
Jay Shin <sjwyk2@gmail.com>
2021-04-10 00:17:29 +0900

    초기 커밋.

---
+++ UltimateBaseball/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UltimateBaseball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+--------------------------------------------------------+");
+            Console.WriteLine("|               궁극의 숫자야구 게임                     |");
+            Console.WriteLine("+--------------------------------------------------------+");
+            Console.WriteLine("| 컴퓨터가 수비수가 되어 세 자릿수를 하나 골랐습니다.    |");
+            Console.WriteLine("| 각 숫자는 0-9중에 하나며 중복되는 숫자는 없습니다.     |");
+            Console.WriteLine("| 모든 숫자의 위치를 맞히면 승리합니다.                  |");
+            Console.WriteLine("| 숫자와 순서가 둘 다 맞으면 스트라이크입니다.           |");
+            Console.WriteLine("| 숫자만 맞고 순서가 틀리면 볼입니다.                    |");
+            Console.WriteLine("| 숫자가 틀리면 아웃입니다.                              |");
+            Console.WriteLine("+--------------------------------------------------------+");
+
+            Console.WriteLine("> 수비수가 고른 숫자");
+            Console.WriteLine(3);
+            Console.WriteLine(1);
+            Console.WriteLine(9);
+        }
+    }
+}
